2ff84851c8073732e397899e87eaa373e6c5b407
havardsollie <79274753+havardsollie@users.noreply.github.com>
2022-11-25 10:42:22 +0100

    del myprofile

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import { AuthProvider } from "./context/AuthContext";
 import Navigate from "./layout/Nav";
 import "./scss/style.scss"
 import ProfileDetails from "./components/detail/ProfileDetail";
-import MyProfile from "./components/profiles/MyProfile";
 import EditPost from "./components/posts/EditPost";
 import ListOfProfiles from "./components/profiles/AllProfiles";
 import Footer from "./layout/Footer";
@@ -25,7 +24,6 @@ function App() {
 						<Route path="/allprofiles" element={<ListOfProfiles />} />
             <Route path="/detail/:id" element={<PostDetails />} />
 						<Route path="/detail/edit/:id" element={<EditPost />} />
-            <Route path="/profile" element={<MyProfile />} />
             <Route path="/profile/:name" element={<ProfileDetails />} />
           
           <Route path="/login" element={<LoginPage />} />
