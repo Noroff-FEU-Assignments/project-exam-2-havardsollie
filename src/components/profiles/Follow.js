ba4c1b39592a77d1e3c881265bac3f741c635647
havardsollie <79274753+havardsollie@users.noreply.github.com>
2022-12-03 18:30:02 +0100

    Commit 3/12

--- src/components/profiles/Follow.js
+++ src/components/profiles/Follow.js
@@ -1,10 +1,8 @@
-import { useState, useContext } from "react";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { BASE_URL } from "../../api/Api";
-import { useNavigate } from "react-router-dom";
 import useAxios from "../../hooks/useAxios";
 import { Button } from "react-bootstrap";
-import AuthContext from "../../context/AuthContext";
 
 function FollowButton() {
 	const [error, setError] = useState(null);
@@ -17,7 +15,6 @@ function FollowButton() {
     try {
       const result = await http.put(url);
       console.log(result);
-      setFollow(true);
     } catch (error) {
       setError(error);
     } finally {
